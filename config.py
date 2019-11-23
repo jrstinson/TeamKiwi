0f46f3e286566bb9ce090da71ea95b2475bf1541
Jacob Stinson <jstinson14@outlook.com>
2019-11-23 15:40:19 -0500

    more gitignore stuff

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = r'PATH_TO_CONTENT_DIR'
+USER_DIR = r'PATH_TO_USER_DIR'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
